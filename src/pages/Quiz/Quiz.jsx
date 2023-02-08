514b29eea2d948e2c3dd56fbf93d03d250b5d135
lanniiee <lav250397@hotmail.com>
2023-02-08 15:06:15 +0000

    add in questions

--- src/pages/Quiz/Quiz.jsx
+++ src/pages/Quiz/Quiz.jsx
@@ -1,11 +1,24 @@
 import "./Quiz.scss";
 import Input from "../../components/Input/Input";
+import {questions} from "../../utils/questions";
+import { useEffect, useState } from "react";
 
 const Quiz = () => {
+    const [question, setQuestion] = useState("");
+
+    const getQuestion = () => {
+        let index = Math.floor(Math.random() * questions.length-1);
+        setQuestion(questions[index]);
+    }
+
+    useEffect(() => {
+        getQuestion()
+    }, [])
 
     return (
         <div className="quiz">
             <h1>Quiz</h1>
+            <p>{question.question}</p>
             <Input />
         </div>
     )
---
+++ src/utils/questions.js
@@ -0,0 +1,14 @@
+export const questions =[
+    {
+        question: "What type of doctor is Doctor Strange?",
+        answer: ["a neurosurgeon", "neurosurgeon"]
+    },
+    {
+        question: "What boy band does RM belong to?",
+        answer: ["bts", "bangtan boys"]
+    },
+    {
+        question: "'I can buy myself flowers, Write my name in the sand, Talk to myself for hours, Say things you don't understand' is the lyrics from what song?",
+        answer: ["flowers"]
+    }
+]
