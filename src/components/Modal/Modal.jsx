b5b0d6f9ccaec04bdd1bf4465756fefd1a1cc98e
lanniiee <lav250397@hotmail.com>
2023-02-09 21:43:34 +0000

    create modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,14 @@
 import "./Modal.scss";
+import PlayerCard from "../PlayerCard/PlayerCard";
 
-const Modal = () => {
+const Modal = ({playerArr, toggleModal}) => {
 
     return (
         <div className="modal">
-
+            <div className="modal__container">
+                <span className="modal__exit" onClick={toggleModal}>x</span>
+                <PlayerCard playerArr={playerArr}/>
+            </div>
         </div>
     )
 
--- src/pages/FootballTeamOrganiser/FootballTeamOrganiser.jsx
+++ src/pages/FootballTeamOrganiser/FootballTeamOrganiser.jsx
@@ -1,21 +1,43 @@
 import "./FootballTeamOrganiser.scss";
-import PlayerCard from "../../components/PlayerCard/PlayerCard";
 import footballersArr from "../../data/football-team.data copy.json";
 import { useState } from "react";
+import Modal from "../../components/Modal/Modal";
 
 const FootballTeamOrganiser = () => {
     const [goalkeepersArr, setGoalkeepersArr] = useState(footballersArr.filter(e => e.position == "Goalkeeper"));
     const [defendersArr, setDefendersArr] = useState(footballersArr.filter(e => e.position == "Defender"));
     const [midfieldersArr, setMidfieldersArr] = useState(footballersArr.filter(e => e.position == "Midfielder"));
+    const [attackersArr, setAttackersArr] = useState(footballersArr.filter(e => e.position == "Attacker"));
+    const [showModal, setShowModal] = useState(false);
+    const [playerArr, setPlayerArr] = useState([]);
 
-    console.log(goalkeepersArr);
-    console.log(defendersArr);
-    console.log(midfieldersArr);
+    const toggleModal = (e) => {
+        
+        if (e.target.textContent == "Select Goalkeeper ▼") {
+            setPlayerArr(goalkeepersArr);
+        } else if (e.target.textContent == "Select Defender ▼") {
+            setPlayerArr(defendersArr);
+        } else if (e.target.textContent == "Select Midfielder ▼") {
+            setPlayerArr(midfieldersArr);
+        } else if (e.target.textContent == "Select Attacker ▼") {
+            setPlayerArr(attackersArr);
+        }
+        setShowModal(!showModal)
+    }
+
 
     return (
         <div className="football">
-            <h1>Football</h1>
-            <PlayerCard playerArr={midfieldersArr}/>
+            <h1 className="football__heading">Football</h1>
+            <div className="football__options">
+                <h1 className="football__select" onClick={toggleModal}>Select Goalkeeper ▼</h1>
+                <h1 className="football__select" onClick={toggleModal}>Select Defender ▼</h1>
+                <h1 className="football__select" onClick={toggleModal}>Select Midfielder ▼</h1>
+                <h1 className="football__select" onClick={toggleModal}>Select Attacker ▼</h1>
+            </div>
+            
+            {showModal && <Modal playerArr={playerArr} toggleModal={toggleModal}/>}
+
         </div>
     )
 }
